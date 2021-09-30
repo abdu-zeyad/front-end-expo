edf2e02b3781fa15eb011edfdae086edbbdc845a
abdelmajed <abd.d@live.com>
2021-09-30 03:02:41 +0300

    s

--- App.js
+++ App.js
@@ -11,34 +11,92 @@ import rootReducer from "./stores/rootReducer";
 
 import Tabs from "./navigation/tabs";
 
-
-
-
-
-
 const Stack = createStackNavigator();
 
 const store = createStore(
   rootReducer,
   applyMiddleware(thunk)
 )
+import LoginScreen from "react-native-login-screen";
+import { View, Text } from 'react-native';
 
 const App = () => {
+  const [x, setX] = React.useState(true)
+
   return (
+
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false
+
+      {x ? <LoginScreen
+        source='bg.jpg'
+        spinnerEnable
+        // spinnerVisibility={spinnerVisibility}
+        labelTextStyle={{
+          color: "#adadad",
+          fontFamily: "Now-Bold",
+
+        }}
+        logoTextStyle={{
+          fontSize: 27,
+          color: "#fdfdfd",
+          fontFamily: "Now-Black",
+        }}
+        loginButtonTextStyle={{
+          color: "#fdfdfd",
+          fontFamily: "Now-Bold",
+        }}
+        textStyle={{
+          color: "#757575",
+          fontFamily: "Now-Regular",
+        }}
+        signupStyle={{
+          color: "#fdfdfd",
+          fontFamily: "Now-Bold",
+        }}
+        usernameOnChangeText={(username) => console.log("Username: ", username)}
+        onPressSettings={() => alert("Settings Button is pressed")}
+        passwordOnChangeText={(password) => console.log("Password: ", password)}
+        onPressLogin={() => {
+          // setSpinnerVisibility(true);
+          // setTimeout(() => {
+          //     setSpinnerVisibility(false);
+          // }, 2000);
+
+
+        }}
+        onPressLogin={() => setX(false)}
+        onPressSignup={() => {
+          console.log("onPressSignUp is pressed");
+        }}
+      >
+        <View
+          style={{
+            position: "relative",
+            alignSelf: "center",
+            marginTop: 64,
+
           }}
-          initialRouteName={'MainLayout'}
         >
-          <Stack.Screen
-            name="MainLayout"
-            component={Tabs}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+          <Text style={{ color: "white", fontSize: 30 }}>
+            Inside Login Screen Component
+          </Text>
+        </View>
+      </LoginScreen> :
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false
+            }}
+            initialRouteName={'MainLayout'}
+          >
+            <Stack.Screen
+              name="MainLayout"
+              component={Tabs}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      }
+
     </Provider>
   )
 }
